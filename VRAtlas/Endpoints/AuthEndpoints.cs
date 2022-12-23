9a1deb1cd434625ff715c958680af7633c97b08b
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-23 06:52:24 -0500

    Remove unused auth endpoints

--- VRAtlas/Endpoints/AuthEndpoints.cs
+++ VRAtlas/Endpoints/AuthEndpoints.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using VRAtlas.Endpoints.Internal;
 using VRAtlas.Models;
 using VRAtlas.Services;
@@ -10,29 +9,9 @@ public class AuthEndpoints : IEndpointCollection
 {
     public static void BuildEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapGet("/auth", GetUser)
-            .RequireAuthorization();
-
-        app.MapGet("/auth/claims", GetClaims);
-        
         app.MapGet("/auth/token", GetUserToken)
             .Produces<UserTokens>(StatusCodes.Status200OK)
             .Produces(StatusCodes.Status401Unauthorized);
-    }
-
-    private static IResult GetUser(HttpContext httpContext)
-    {
-        _ = httpContext;
-        return Results.Ok(new { Hello = "World" });
-    }
-
-    private static IResult GetClaims(ClaimsPrincipal principal)
-    {
-        return Results.Ok(principal.Claims.Select(c => new
-        {
-            c.Type,
-            c.Value
-        }));
     }
 
     private static async Task<IResult> GetUserToken([FromQuery] string code, [FromQuery] Uri redirectUri, IAuthService authService, IUserGrantService userGrantService)

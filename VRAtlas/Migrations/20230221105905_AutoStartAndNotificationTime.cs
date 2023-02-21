f52f8264f8ee14dbee89e9fe5a16bd7a8b1ea6b9
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-02-21 06:00:00 -0500

    (EF) Auto Start and Notification Creation Time

---
+++ VRAtlas/Migrations/20230221105905_AutoStartAndNotificationTime.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace VRAtlas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoStartAndNotificationTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Instant>(
+                name: "CreatedAt",
+                table: "Notifications",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: NodaTime.Instant.FromUnixTimeTicks(0L));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "Notifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoStart",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "AutoStart",
+                table: "Events");
+        }
+    }
+}
